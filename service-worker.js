b0501b1f8162abda4ecc3a4dc09690827d4fa68d
TASABelize <development@tasabelize.com>
2025-08-19 14:00:20 -0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,15 @@
+const CACHE_NAME = 'fishing-app-cache-v3';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/logo.png'
+];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+});
